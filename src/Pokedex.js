87c4b4d002ced8081a8da16f50545964a757ba66
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-11-04 00:22:00 -0300

    exercicio 2 ok

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,28 +4,58 @@ import Pokemon from './Pokemon';
 class Pokedex extends React.Component {
     constructor() {
         super();
-        this.state = { indice: 0 };
+        this.state = {
+            tipos: ['Fire', 'Psychic'],
+            atualizar: false,
+        };
         this.proximo = this.proximo.bind(this);
+        this.filtro = this.filtro.bind(this);
+    }
+    
+    filtro(f) {
+        localStorage.setItem('filtro', f);
+        this.setState(estadoAnt => Object.assign({}, estadoAnt, {atualizar: !estadoAnt.atualizar}));
     }
 
     proximo() {
-        if (this.props.pokemons.length-1 > this.state.indice) {
-            this.setState((estadoAnterior) => ({indice: estadoAnterior.indice + 1}));
-        } else { this.setState((estadoAnterior) => ({ indice: 0 }))}
+        this.setState(estadoAnt => Object.assign({}, estadoAnt, {atualizar: !estadoAnt.atualizar}));
+        localStorage.setItem('indice', parseInt(localStorage.getItem('indice')) + 1);
     }
-
+        
     render() {
+        let poke = this.props.pokemons[0];
+        if (!Object.keys(localStorage).includes('filtro')) {
+            localStorage.setItem('filtro', 'All');
+            localStorage.setItem('indice', '0');
+        }
+        else {
+            if (localStorage.getItem('filtro') !== 'All') {
+                const pokes = this.props.pokemons.filter( p => p.type === localStorage.getItem('filtro'));
+                if (pokes.length <= localStorage.getItem('indice')) localStorage.setItem('indice', '0');
+                poke = pokes[localStorage.getItem('indice')];
+            } else {
+                const pokes = this.props.pokemons;
+                if (pokes.length <= localStorage.getItem('indice')) localStorage.setItem('indice', '0');
+                poke = pokes[localStorage.getItem('indice')]
+            }
+        }
+
         return (
-            <div>
+            <>
                 <div className="pokedex">
-                    {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
                     <Pokemon 
-                        key={this.props.pokemons[this.state.indice]} 
-                        pokemon={this.props.pokemons[this.state.indice]} 
+                        key={poke}
+                        pokemon={poke}
                         />
                 </div>
-                <button onClick={this.proximo}>Próximo</button>
-            </div>
+                <div className="botoes">
+                    <div className="tipos">{
+                        this.state.tipos.map((p) => <button onClick={() => this.filtro(p)} key={p} >{p}</button>)
+                        }
+                    </div>
+                    <button onClick={this.proximo}>Próximo</button>
+                </div>
+            </>
         );
     }
 }
